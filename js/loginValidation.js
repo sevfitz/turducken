dc16a84300c3c072e60911be696e5942f246f4a4
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-05-23 14:40:51 -0700

    Login validation

--- js/loginValidation.js
+++ js/loginValidation.js
@@ -1,2 +1,17 @@
 'use strict';
 
+function noSpace( loginInput ) {
+    if ( loginInput.value.match(/\s/g) ){
+        alert( 'Spaces are not allowed in usernames or passwords.' );
+        loginInput.value = loginInput.value.replace( /\s/g, '' );
+    }
+}
+
+var uNameEl = document.getElementById( 'uName' );
+var uPassEl = document.getElementById( 'uPass' );
+uNameEl.addEventListener( 'blur', function () {
+    noSpace( uNameEl );
+});
+uPassEl.addEventListener( 'blur', function () {
+    noSpace( uPassEl );
+});
