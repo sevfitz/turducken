456670df8f978c71f0db55e7931db886726a1de0
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-05-23 15:48:30 -0700

    User object in loginValidation for testing storage

--- js/loginValidation.js
+++ js/loginValidation.js
@@ -1,5 +1,36 @@
 'use strict';
 
+function User( fName, lName, uName, uPass, img ) {
+    this.fName = fName;
+    this.lName = lName;
+    this.uName = uName;
+    this.uPass = uPass;
+    this.img = img;
+    this.posts = [];    
+}
+
+var testUser = new User('First','Last','testuName','testuPass','../img/cartoon-hedgehog.jpg');
+
+User.prototype.saveLogin = function() {
+    // save the uName and uPass in localStorage
+    localStorage.setItem( 'uName', JSON.stringify(this.uName));
+    localStorage.setItem( 'uPass', JSON.stringify(this.uPass));
+
+};
+
+User.prototype.checkLogin = function() {
+    // check if the uName and uPass are in local storage
+    // retrieve uName and uPass from local storage if so
+    // check if the uName and uPass match
+    // login if both are true
+
+
+
+};
+
+
+
+
 function noSpace( loginInput ) {
     if ( loginInput.value.match(/\s/g) ){
         alert( 'Spaces are not allowed in usernames or passwords.' );
@@ -15,3 +46,4 @@ uNameEl.addEventListener( 'blur', function () {
 uPassEl.addEventListener( 'blur', function () {
     noSpace( uPassEl );
 });
+
