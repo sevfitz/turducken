d4d7dbfd0e9aa9ad9e813cdd54bae0f4c8fa5190
Haley Tortorella <hdtort@gmail.com>
2017-05-24 14:53:48 -0700

    my bad guys

--- js/twitter.js
+++ js/twitter.js
@@ -1,13 +1,13 @@
 'use strict';
 
-User.prototype.newTweet = function (tweet) {
-    var tweet = JSON.parse(localStorage.getItem('tweet'));
-    this.posts.push(new Post(tweet, 'Twitter'));
-
-};
+// User.prototype.newTweet = function (tweet) {
+//     if (tweet) {
+//         var tweet = JSON.parse(localStorage.getItem('tweet'));
+//         this.posts.push(new Post(tweet, 'Twitter'));
+//     }
+// };
 
 var tweetEl = document.getElementById('textArea');
-
 
 var twitterBut = document.getElementById('twitterBut');
 twitterBut.addEventListener('click', function(){
@@ -16,5 +16,8 @@ twitterBut.addEventListener('click', function(){
 
 function saveToStorage(post) {
     localStorage.setItem('tweet', JSON.stringify(post));
-};
+}
 
+
+var tweet = JSON.parse(localStorage.getItem('tweet'));
+this.posts.push(new Post(tweet, 'Twitter'));
